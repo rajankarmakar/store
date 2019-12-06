961b11e382aa87f77eb04c25b3525c6fa89b309c
rajankarmakar <rajankarmakar@hotmail.com>
2019-12-06 12:36:31 +0600

    Updates

---
+++ precache-manifest.623ecd18a7316ef0b292251bbdd8f9c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccb0cb866207801f29bf7eb83db197e9",
+    "url": "/store/index.html"
+  },
+  {
+    "revision": "c6677440a779da9556c2",
+    "url": "/store/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "ea73f10481994344ea87",
+    "url": "/store/static/css/main.e9639a40.chunk.css"
+  },
+  {
+    "revision": "c6677440a779da9556c2",
+    "url": "/store/static/js/2.9b62b45e.chunk.js"
+  },
+  {
+    "revision": "ea73f10481994344ea87",
+    "url": "/store/static/js/main.578c9d63.chunk.js"
+  },
+  {
+    "revision": "e03b94b63ad0ff8b4326",
+    "url": "/store/static/js/runtime~main.296ec49c.js"
+  }
+]);
